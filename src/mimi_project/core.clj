0168c1c2c5404003893956d93139b723262e1d7c
RafaelCampo-ME <rs.campo10@uniandes.edu.co>
2022-10-07 10:50:07 -0500

    INITIAL COMMIT

---
+++ src/mimi_project/core.clj
@@ -0,0 +1,21 @@
+(ns mimi-project.core
+  (:gen-class) 
+  (:use incanter.core)
+  (:use incanter.io)
+  )
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!") 
+
+  (read-dataset "/mimi-project/doc/prueba.csv" :header true)
+)
+
+(-main)
+
+
+(defn demo [x y] (* 2 x y))
+(demo 2 3)
+
+(demo 2 (demo 2 3))
