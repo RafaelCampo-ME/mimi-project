ae77ec2f4cb63a8b3945120c320499f865d10650
Rafael <rs.campo10@uniandes.edu.co>
2022-10-13 07:50:25 -0500

    ca

--- src/mimi_project/ejemplos.clj
+++ src/mimi_project/ejemplos.clj
@@ -1,9 +1,12 @@
 (ns mimi-project.ejemplos)
 
-(defn connect-db [] 
+(defn connect-db []
   ;;Recursive function in clojure
   (println "Trying to connect")
   (let [connected (>= (rand-int 5) 4)]
     (if connected "connected" (connect-db))))
 
 (connect-db)
+
+ 
+(. System (nanoTime)) 
