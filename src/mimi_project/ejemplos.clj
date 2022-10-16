73f22effef4bb232c94fbc5acfdf23e1e9a47cbd
RafaelCampo-ME <rs.campo10@uniandes.edu.co>
2022-10-15 20:36:11 -0500

    Change in methods

--- src/mimi_project/ejemplos.clj
+++ src/mimi_project/ejemplos.clj
@@ -1,12 +1,9 @@
 (ns mimi-project.ejemplos)
 
-(defn connect-db []
+(defn connect-db [] 
   ;;Recursive function in clojure
   (println "Trying to connect")
   (let [connected (>= (rand-int 5) 4)]
     (if connected "connected" (connect-db))))
 
 (connect-db)
-
- 
-(. System (nanoTime)) 
