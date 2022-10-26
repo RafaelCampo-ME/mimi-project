d748b5d4058d4cf5b59f5a7a98b8fd4a1d517894
Rafael <rs.campo10@uniandes.edu.co>
2022-10-25 23:04:08 -0500

    Creation of Fourier file

---
+++ src/mimi_project/fourier.clj
@@ -0,0 +1,19 @@
+(ns mimi-project.fourier)
+
+;;This method will implement a fourier serie aproximation of a given function
+
+(identical? "foo" "foo")
+
+(sorted-map :b 20 :a 10 :c 30 :d 40)
+
+;;;Que interesante el sorted map me organiza de una vez mis mapas
+ 
+
+;;Ejemplos de uso de seq
+
+(first (seq [:a :b :c :d]))
+
+(rest (seq [:a :b :c :d]))
+
+(next (seq [:a :b :c :d]))
+
